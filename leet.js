719fac3b7074041b3c1d2566df6252f572587d86
Amalia Hay <amaliawhay@gmail.com>
2021-04-10 13:54:20 -0700

    pseudocode

--- leet.js
+++ leet.js
@@ -3,3 +3,11 @@ Given an m x n integers matrix, return the length of the longest increasing path
 
 From each cell, you can either move in four directions: left, right, up, or down. You may not 
 move diagonally or move outside the boundary (i.e., wrap-around is not allowed).*/
+
+//Find longest increasing path for each point
+
+//Set directions (up, down, l, r)
+
+//Check matrix value (newx, newy > x, y)
+
+//Increase path (output) +1 with each iteration
